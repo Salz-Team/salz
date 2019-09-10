81eccfacb0b9d3b344f06d76cca2ce3d52fc7fd8
Lukas <lfschmid@uwaterloo.ca>
2019-09-10 18:29:18 -0400

    Basic Docker Postgres setup

---
+++ Database/docker-entrypoint-initdb.d/player.sql
@@ -0,0 +1,5 @@
+create table players (playerid integer primary key, 
+                      username character(8),
+                      currentBotDir character(100),
+                      previousBotDir character(100),
+                      botstatus character(8))
---
+++ Database/rundb.sh
@@ -0,0 +1,1 @@
+docker run -p 5432:5432  -d salz-db
