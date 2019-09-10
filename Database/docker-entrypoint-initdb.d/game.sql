709e1e3a58e2c29d39ab9b672b61e8dcbc53c78e
Lukas <noemail@no.com>
2019-09-10 18:36:13 -0400

    Added the Game schema to the database

---
+++ Database/docker-entrypoint-initdb.d/game.sql
@@ -0,0 +1,6 @@
+create table game (id integer primary key, 
+                   turnId integer,
+                   x integer,
+                   y integer,
+                   playerid integer,
+                   timestamp timestamp)
