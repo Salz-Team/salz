9b292e3b2d64c1d7da01da64e9fc00d9284e7b59
Dillon Chan <dillonchan97@gmail.com>
2019-09-10 18:34:39 -0400

    first commit: basic flask server

---
+++ GameEngine/src/salz-api/salz-api.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "suh b"
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port='8080')
+
