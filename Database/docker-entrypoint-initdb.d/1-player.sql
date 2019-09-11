40da1d5dfff00a598f2158e8581e5e076bc8565a
Dillon Chan <dillonchan97@gmail.com>
2019-09-11 17:14:18 -0400

    Clean up SQL, add FK constraints

---
+++ Database/docker-entrypoint-initdb.d/1-player.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS players (playerid INTEGER PRIMARY KEY, 
+                      username CHARACTER(8),
+                      currentBotDir CHARACTER(100),
+                      previousBotDir CHARACTER(100),
+                      botstatus CHARACTER(8))
