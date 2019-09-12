4d034ef3816ca090ce2df4d0c76f664360ad2c24
Dillon Chan <dillonchan97@gmail.com>
2019-09-12 19:53:29 -0400

    Replaced sqlalchemy with pony

--- GameEngine/src/salz-api/salz_api.py
+++ GameEngine/src/salz-api/salz_api.py
@@ -1,46 +1,43 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import psycopg2
+from pony.orm import *
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:mysecretpassword@localhost:5432"
 
-# put this somewhere safe later
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:mysecretpassword@localhost:5432"
+db = Database()
+db.bind(provider='postgres', user='postgres', password='mysecretpassword', host='localhost', port=5432, database='postgres')
 
-db = SQLAlchemy(app)
+set_sql_debug(True)
 
-class Game(db.Model):
-    __tablename__ = 'game'
-    id = db.Column(db.Integer, primary_key=True)
-    turnId = db.Column(db.Integer)
-    x = db.Column(db.Integer)
-    y = db.Column(db.Integer)
-    playerid = db.Column(db.Integer)
-    generated_at = db.Column(db.DateTime)
+# classes corresponding to tables in the db
+class Game(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    turnid = Required(int)
+    x = Required(int)
+    y = Required(int)
+    playerid = Required(int)
+    generated_at = Required(datetime)
 
+class Players(db.Entity):
+    playerid = PrimaryKey(int, auto=True)
+    username = Required(str)
+    botdir = Optional(str)
+    updatedbot = Required(bool)
+    newbotdir = Optional(str)
+    botstatus = Optional(str)
 
-class Players(db.Model):
-    playerid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.VARCHAR)
-    botdir = db.Column(db.VARCHAR)
-    updatedbot = db.Column(db.BOOLEAN)
-    newbotdir = db.Column(db.VARCHAR)
-    botstatus = db.Column(db.VARCHAR)
+# so pony knows how the Game class relates to the Game table
+db.generate_mapping()
 
+# things accessing the db need to have the db_session decorator
 @app.route('/')
+@db_session
 def hello_world():
 
-    testthing = Players.query.all()
+    users = select(p.username for p in Players)[:]
 
-    users = []
-    print(testthing)
-    for p in testthing:
-        print(p.username)
-        users.append(p.username)
-
-    return f"the current users are {users}"
-
+    return f"{users}"
 
 if __name__ == '__main__':
     app.debug = True
