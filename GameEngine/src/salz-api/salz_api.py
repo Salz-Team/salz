419148ec4d39b39af62dbedafb74cdf0e46290c7
Dillon Chan <dillonchan97@gmail.com>
2019-09-11 22:22:01 -0400

    Proof of concept: connect to postgres and query users

--- GameEngine/src/salz-api/salz_api.py
+++ GameEngine/src/salz-api/salz_api.py
@@ -1,12 +1,16 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 
-from salz_api import app
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:mysecretpassword@localhost:5432"
 
+# put this somewhere safe later
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:mysecretpassword@localhost:5432"
 
 db = SQLAlchemy(app)
 
-class GameModel(db.Model):
+class Game(db.Model):
     __tablename__ = 'game'
     id = db.Column(db.Integer, primary_key=True)
     turnId = db.Column(db.Integer)
@@ -16,11 +20,29 @@ class GameModel(db.Model):
     generated_at = db.Column(db.DateTime)
 
 
-class PlayerModel(db.Model):
+class Players(db.Model):
     playerid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.VARCHAR)
     botdir = db.Column(db.VARCHAR)
-    updatedbot = db.Column(db.Bool)
+    updatedbot = db.Column(db.BOOLEAN)
     newbotdir = db.Column(db.VARCHAR)
     botstatus = db.Column(db.VARCHAR)
+
+@app.route('/')
+def hello_world():
+
+    testthing = Players.query.all()
+
+    users = []
+    print(testthing)
+    for p in testthing:
+        print(p.username)
+        users.append(p.username)
+
+    return f"the current users are {users}"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port='8080')
 
--- GameEngine/src/salz-api/taskell.md
+++ GameEngine/src/salz-api/taskell.md
@@ -8,9 +8,10 @@
 
 - Connect to postgres db
     > look at https://www.codementor.io/dongido/how-to-build-restful-apis-with-python-and-flask-fh5x7zjrx for inspiration. 
-    * [ ] Schema should have NOT NULLS
-    * [ ] Schema should have game.playerid as foreignkey
+    * [x] Schema should have NOT NULLS
+    * [x] Schema should have game.playerid as foreignkey
     * [ ] Set up a docker-compose with api and database with makefile
+    * [x] just set up that sqlalchemy - postgres connection. make a proof of concept
 
 ## Done
 
