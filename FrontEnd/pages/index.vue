37ee88d2b918de8f89d0faff96e8623f3dfcfa21
Japorized <johnson5756@live.com>
2019-09-12 22:22:59 -0400

    reorganized and replaced navbar

--- FrontEnd/pages/index.vue
+++ FrontEnd/pages/index.vue
@@ -1,34 +1,6 @@
 <template>
   <section class="section">
     <div class="is-mobile">
-      <!-- <card title="Free" icon="github-circle"> -->
-      <!--   Open source on -->
-      <!--   <a href="https://github.com/buefy/buefy"> -->
-      <!--     GitHub -->
-      <!--   </a> -->
-      <!-- </card> -->
-
-      <!-- <card title="Responsive" icon="cellphone-link"> -->
-      <!--   <b class="has-text-grey"> -->
-      <!--     Every -->
-      <!--   </b> -->
-      <!--   component is responsive -->
-      <!-- </card> -->
-
-      <!-- <card title="Modern" icon="alert-decagram"> -->
-      <!--   Built with -->
-      <!--   <a href="https://vuejs.org/"> -->
-      <!--     Vue.js -->
-      <!--   </a> -->
-      <!--   and -->
-      <!--   <a href="http://bulma.io/"> -->
-      <!--     Bulma -->
-      <!--   </a> -->
-      <!-- </card> -->
-
-      <!-- <card title="Lightweight" icon="arrange-bring-to-front"> -->
-      <!--   No other internal dependency -->
-      <!-- </card> -->
       <h1 class="title is-1">Salz</h1>
 
       <p>A bot based game.</p>
@@ -44,13 +16,7 @@
 </template>
 
 <script>
-// import Card from '~/components/Card';
-
 export default {
   name: 'HomePage'
-
-  // components: {
-  //   Card
-  // }
 };
 </script>
