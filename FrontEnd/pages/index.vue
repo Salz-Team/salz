976e1c0b498f378cd9038b7e73ba4070f1b46d8e
Japorized <johnson5756@live.com>
2019-09-13 23:17:34 -0400

    made game layout take up entire space

--- FrontEnd/pages/index.vue
+++ FrontEnd/pages/index.vue
@@ -1,18 +1,20 @@
 <template>
-  <section class="section">
-    <div class="is-mobile">
-      <h1 class="title is-1">Salz</h1>
+  <div class="container column is-12">
+    <section class="section">
+      <div class="is-mobile">
+        <h1 class="title is-1">Salz</h1>
 
-      <p>A bot based game.</p>
+        <p>A bot based game.</p>
 
-      <h2 class="title is-2">Bot Requirements</h2>
+        <h2 class="title is-2">Bot Requirements</h2>
 
-      <p>
-        Bots should be submitted as a .tar file containing a
-        <code>run.sh</code> and a <code>build.sh</code> in the root directory.
-      </p>
-    </div>
-  </section>
+        <p>
+          Bots should be submitted as a .tar file containing a
+          <code>run.sh</code> and a <code>build.sh</code> in the root directory.
+        </p>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
