bc821a3c5870c36aa32149fe57b7cce6e4613bd5
Japorized <johnson5756@live.com>
2019-09-20 18:05:06 -0400

    decouple fullscreening function from page

--- FrontEnd/pages/game.vue
+++ FrontEnd/pages/game.vue
@@ -9,30 +9,26 @@
 </style>
 
 <script charset="utf-8">
+import { fullscreen } from '../lib/game-rendering/fullscreen';
+
 export default {
   data() {
     return {
       index: []
     };
   },
-  async asyncData(ctx) {
-    return {
-      index: await ctx.app.$framesRepo.index()
-    };
-  },
+  // async asyncData(ctx) {
+  //   return {
+  //     // index: await ctx.app.$framesRepo.index()
+  //   };
+  // },
   async mounted() {
     const PIXI = await import('pixi.js');
     const Viewport = await import('pixi-viewport');
-    this.index.frames.forEach((frame) => {
-      console.log(frame['1']);
-    });
+    // this.index.frames.forEach((frame) => {
+    //   console.log(frame['1']);
+    // });
 
-    document.cancelFullScreen =
-      document.cancelFullScreen ||
-      document.webkitCancelFullScreen ||
-      document.mozCancelFullScreen;
-
-    let isFullscreen = false;
     const wrapper = document.querySelector('#salz-game-inner-view');
 
     const appWidth = Math.max(
@@ -77,44 +73,68 @@ export default {
       });
 
     // hotkeys
-    document.onkeypress = function(e) {
+    document.onkeydown = function(e) {
       switch (e.keyCode) {
-        case 102: // f
-          fullscreen(app.view, !isFullscreen);
+        case 70: // f
+          fullscreen(app.view);
+          break;
+        case 37: // left
+          viewport.removeChildren(0, viewport.length);
+          break;
+        case 39: // right
           break;
       }
     };
 
-    const btn = createTextButton(
-      'Fullscreen',
-      { padding: 10, fill: '#FF0000' },
-      () => {
-        fullscreen(app.view, !isFullscreen);
-      }
-    );
-    btn.x = appWidth - btn.width - 20;
-    btn.y = appHeight - btn.height - 20;
+    // const btn = createTextButton(
+    //   'Fullscreen',
+    //   { padding: 10, fill: '#FF0000' },
+    //   () => {
+    //     fullscreen(app.view);
+    //   }
+    // );
+    // btn.x = appWidth - btn.width - 20;
+    // btn.y = appHeight - btn.height - 20;
 
     // This is a dummy data set
-    const frame = [
-      {
-        playerid: 0,
-        color: '0xff0000',
-        coords: [{ x: 0, y: 1 }, { x: 20, y: 50 }, { x: 50, y: 20 }]
-      },
-      {
-        playerid: 1,
-        color: '0x00ff00',
-        coords: [{ x: 50, y: 0 }, { x: 0, y: 50 }, { x: 20, y: 20 }]
-      },
-      {
-        playerid: 2,
-        color: '0x0000ff',
-        coords: [{ x: -20, y: 0 }, { x: 0, y: -50 }, { x: 20, y: -20 }]
-      }
+    const frames = [
+      [
+        {
+          playerid: 0,
+          color: '0xff0000',
+          coords: [{ x: 0, y: 1 }, { x: 20, y: 50 }, { x: 50, y: 20 }]
+        },
+        {
+          playerid: 1,
+          color: '0x00ff00',
+          coords: [{ x: 50, y: 0 }, { x: 0, y: 50 }, { x: 20, y: 20 }]
+        },
+        {
+          playerid: 2,
+          color: '0x0000ff',
+          coords: [{ x: -20, y: 0 }, { x: 0, y: -50 }, { x: 20, y: -20 }]
+        }
+      ],
+      [
+        {
+          playerid: 0,
+          color: '0xff0000',
+          coords: [{ x: 1, y: 1 }, { x: 21, y: 50 }, { x: 51, y: 20 }]
+        },
+        {
+          playerid: 1,
+          color: '0x00ff00',
+          coords: [{ x: 51, y: 0 }, { x: 1, y: 50 }, { x: 21, y: 20 }]
+        },
+        {
+          playerid: 2,
+          color: '0x0000ff',
+          coords: [{ x: -19, y: 0 }, { x: 1, y: -50 }, { x: 21, y: -20 }]
+        }
+      ]
     ];
 
-    frame.forEach(function(player) {
+    frames[0].forEach(function(player) {
       drawCells(player.coords, player.color);
     });
 
@@ -131,49 +151,30 @@ export default {
       sprite.position.set(coord.x * 10, coord.y * 10);
     }
 
-    function fullscreen(el, value) {
-      el.addEventListener('webkitfullscreenchange', onFullscreenChange);
-      el.addEventListener('mozfullscreenchange', onFullscreenChange);
-      el.addEventListener('fullscreenchange', onFullscreenChange);
+    // to be refactored so that it can be used with image assets
+    // function createTextButton(text, style, fn) {
+    //   const btn = new PIXI.Text(text, style);
+    //   btn.interactive = true;
+    //   btn.buttonMode = true;
+    //   btn.on('click', fn);
+    //   app.stage.addChild(btn);
 
-      if (value) {
-        if (el.webkitRequestFullscreen) {
-          el.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-        } else {
-          el.mozRequestFullScreen();
-        }
-      } else {
-        document.cancelFullScreen();
-      }
-    }
-
-    function onFullscreenChange() {
-      isFullscreen = !isFullscreen;
-    }
-
-    function createTextButton(text, style, fn) {
-      const btn = new PIXI.Text(text, style);
-      btn.interactive = true;
-      btn.buttonMode = true;
-      btn.on('click', fn);
-      app.stage.addChild(btn);
-
-      return btn;
-    }
+    //   return btn;
+    // }
 
     window.addEventListener('resize', (event) => {
       app.resize();
 
-      const appWidth = Math.max(
-        document.documentElement.clientWidth,
-        window.innerWidth || 0
-      );
-      const appHeight = Math.max(
-        document.documentElement.clientHeight,
-        window.innerHeight || 0
-      );
-      btn.x = appWidth - btn.width - 20;
-      btn.y = appHeight - btn.height - 20;
+      // const appWidth = Math.max(
+      //   document.documentElement.clientWidth,
+      //   window.innerWidth || 0
+      // );
+      // const appHeight = Math.max(
+      //   document.documentElement.clientHeight,
+      //   window.innerHeight || 0
+      // );
+      // btn.x = appWidth - btn.width - 20;
+      // btn.y = appHeight - btn.height - 20;
     });
   }
 };
