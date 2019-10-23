24201642e79499648f58f5c55d5e7ea7fa2b4a3b
Lukas <lfschmid@uwaterloo.ca>
2019-10-22 21:34:35 -0400

    [game-engine] Make engine try to read from environment variables

--- salz-engine/app/Main.hs
+++ salz-engine/app/Main.hs
@@ -1,7 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import System.Environment
+import Control.Exception
+import Data.Text
 import Game
 
 main :: IO ()
-main = startGameEngine "host='salz-db' port=5432 dbname='postgres' user='postgres' password='mysecretpassword'"
+main = connectionString >>= startGameEngine
+
+connectionString :: IO Text
+connectionString = do
+  envVars <- catch loadEnvVariables ((\_ -> pure []) :: IOError -> IO [Text])
+
+  if envVars == []
+  then
+    return "host='localhost' port=5432 dbname='postgres' user='postgres' password='mysecretpassword'"
+  else
+    return $ "host='" `append` (envVars!!4) `append`
+             "' port=" `append` (envVars!!3) `append`
+             " dbname='" `append` (envVars!!0) `append`
+             "' user='" `append` (envVars!!1) `append`
+             "' password='" `append` (envVars!!2) `append`
+             "'"
+
+loadEnvVariables :: IO [Text]
+loadEnvVariables = mapM (\x -> pack <$> getEnv x) ["POSTGRES_DB"
+                                                  ,"POSTGRES_USER"
+                                                  ,"POSTGRES_PASSWORD"
+                                                  ,"POSTGRES_PORT"
+                                                  ,"POSTGRES_HOST"]
