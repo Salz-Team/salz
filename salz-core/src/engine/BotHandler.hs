6ed33ccfb8d31275cc2eacc880113e66a14a59a5
Lukas Schmidt <lfschmidt.me@gmail.com>
2020-04-12 12:43:36 -0400

    [salz-core] Restructured the bot handler as a monad

--- salz-core/src/engine/BotHandler.hs
+++ salz-core/src/engine/BotHandler.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, MultiParamTypeClasses #-}
 module BotHandler
   ( Bot(..)
   , takeTurn
@@ -17,13 +17,19 @@ import System.Timeout
 import System.IO
 import System.Exit
 import GHC.IO.Handle
+import GHC.IO.Exception
+import Data.List
+import Control.Monad.Trans.Class
+import Control.Monad.State.Class (MonadState(..))
+import Control.Monad.IO.Class
+import Data.Maybe
 
 import qualified Map as Map
 
 data Bot = Crashed { playerId :: Int
-                   , errorLog :: T.Text
-                   , memory :: T.Text
-                   , errorMsg :: T.Text
+                   , errorLog :: String
+                   , memory :: String
+                   , errorMsg :: String
                    }
           | NewBot { playerId :: Int
                    , filePath :: FilePath
@@ -31,95 +37,118 @@ data Bot = Crashed { playerId :: Int
                    }
           | Bot    { playerId :: Int
                    , filePath :: FilePath
-                   , memory :: T.Text
-                   , errorLog :: T.Text
+                   , memory :: String
+                   , errorLog :: String
                    , commands :: [(Int, Int)]
+                   , mapReq :: Maybe ((Int, Int), Int)
                    } deriving Show
 
+isCrashed :: Bot -> Bool
+isCrashed (Crashed _ _ _ _) = True
+isCrashed _ = False
+
+newtype BotT m a = BotT { runBotT :: Bot -> m (Maybe a, Bot) }
 
 
-data RunError = RunError Bot
-  deriving Show
+instance (Functor m) =>  Functor (BotT m) where
+  fmap f (BotT sf) = BotT $ \s -> (\(a, ns) -> (f <$> a, ns)) <$> (sf s)
 
-instance CE.Exception RunError
+instance (Monad m) => Applicative (BotT m) where
+  pure a = BotT $ \s -> case () of
+    _ | isCrashed s -> return (Nothing, s)
+      | otherwise   -> return (Just a, s)
 
-tShow :: Show a => a -> T.Text
-tShow = T.pack . show
+  (BotT mf) <*> (BotT ma) = BotT $ \s -> do
+      (f, s') <- mf s
+      maybe (return (Nothing, s')) (\f' -> fmap (\(a, s'') -> (f' <$> a, s'')) (ma s')) f
 
-fromJustException :: Maybe a -> T.Text -> IO a
-fromJustException (Just a) _ = return a
-fromJustException Nothing er = CE.throwIO (RunError er)
+instance (Monad m) => Monad (BotT m) where
+  return a = pure a
+  (BotT ma) >>= mf = BotT $ \s -> do
+    (a, s') <- ma s
+    maybe (return (Nothing, s')) (\a' -> runBotT (mf a') s') a
 
-timedCallandResponse :: Int -> Handle -> Handle -> T.Text -> IO T.Text
-timedCallandResponse time stdin stdout call = do
-  hPutStrLn stdin (T.unpack call)
-  hFlush stdin
+instance (Monad m) => MonadState Bot (BotT m) where
+  get = BotT $ \s -> return (Just s, s)
+  put s' = BotT $ \s -> case () of
+    _ | isCrashed s -> return (Nothing, s)
+      | otherwise   -> return (Just (), s')
 
-  response <- timeout time (T.pack <$> (hGetLine stdout))
-  fromJustException response ("Time ran out on call: " `T.append` call)
+instance (MonadIO m) => MonadIO (BotT m) where
+  liftIO ma = BotT $ \s -> case () of
+    _ | isCrashed s -> return (Nothing, s)
+      | otherwise   -> do
+          a <- liftIO $ CE.try ma
+          case a of
+            (Left err) -> return (Nothing, handleIOError s err)
+            (Right res) -> return (Just res, s)
+    where
+      handleIOError :: Bot -> IOError -> Bot
+      handleIOError (Bot pid _ mem errlog _ _) err = Crashed pid errlog mem (GHC.IO.Exception.ioe_description err)
+      handleIOError (NewBot pid _ _) err = Crashed pid "" "" (GHC.IO.Exception.ioe_description err)
+      handleIOError a _ = a
 
-timedWaitExitCode :: Int -> Process Handle Handle Handle -> IO ExitCode
-timedWaitExitCode time process = do
-  exitCode <- timeout time (waitExitCode process)
-  fromJustException exitCode "Time ran out waiting for bot to shutdown"
+killBot :: (Monad m) => BotT m a
+killBot = BotT $ \s -> return (Nothing, s)
 
-tryRunError :: IO Bot -> IO Bot
-tryRunError b = E.either (\(RunError bot) -> bot) id <$> CE.try b
-
-initializeBot :: Bot -> IO Bot
-initializeBot (Crashed a b c d) = return (Crashed a b c d)
-initializeBot (Bot a b c d e) = return (Bot a b c d e)
-initializeBot (NewBot pid fp sloc) = withProcessWait botConfig $ \process -> do
-    let call = T.intercalate " " ["Initialize", tShow pid, tShow (fst sloc), tShow (snd sloc)]
-    memory <- timedCallandResponse 100 (getStdin process) (getStdout process) call
-    hClose (getStdin process)
-
-
-    exitCode <- timedWaitExitCode 100 process
-
-    errormsg' <- T.pack <$> hGetContents (getStderr process)
-    errormsg <- CE.evaluate $ force errormsg'
-
-    return $ Bot { filepath = fp
-                 , playerId = pid
-                 , memory = memory
-                 , errorLog = errormsg
-                 , commands = []
-                 }
+tryIO :: String -> IO (Maybe a) -> (BotT IO a)
+tryIO errmsg ma = do
+  a <- liftIO ma
+  bot <- get
+  case a of
+    Nothing -> put (handleError bot errmsg) >> killBot
+    Just a' -> return a'
   where
-    fp = filepath bot
-    sloc = startingLocation bot
-    pid = playerId bot
+    handleError :: Bot -> String -> Bot
+    handleError (Bot pid _ mem errlog _ _) ermsg = Crashed pid errlog mem ermsg
+    handleError (NewBot pid _ _) ermsg = Crashed pid "" "" ermsg
+    handleError a _ = a
+
+
+execBot :: Process a b c -> Bot -> BotT IO () -> IO Bot
+execBot p b botM = snd <$> runBotT botM b <* stopProcess p
+
+initialize :: Bot -> IO Bot
+initialize (Crashed a b c d) = return (Crashed a b c d)
+initializeBot (Bot a b c d e f) = return (Bot a b c d e f)
+initializeBot b = withProcessWait botConfig $ \p -> execBot p b $ do
+  bot <- get
+  liftIO $ hPutStrLn (getStdin p) $ intercalate " " [ "Initialize"
+                                                    , show (playerId bot)
+                                                    , show (fst $ startingLocation bot)
+                                                    , show (snd $ startingLocation bot)
+                                                    , "\n"
+                                                    ]
+  liftIO $ hFlush (getStdin p)
+
+  response <- tryIO "Timeout while waiting for initial response." $ timeout 1000 (hGetLine (getStdout p))
+  put $ Bot (playerId bot) (filePath bot) response "" [] Nothing
+
+  return ()
+  where
     botConfig = setStdin createPipe
               $ setStdout createPipe
               $ setStderr createPipe
-              $ setWorkingDir (dropFileName fp)
-              $ fromString fp
+              $ setWorkingDir (dropFileName (filePath b))
+              $ fromString (filePath b)
 
-takeTurn :: Map.Map -> BotHandler -> IO Bot
-takeTurn world (NewBot fp pid sloc) = initializeBot (NewBot fp pid sloc) >>= takeTurn world
-takeTurn world (Bot fp _ mem _ _ ) = tryRunError $ withProcessWait botConfig $ \process -> do
-    mapReq <- timedCallandResponse 100 (getStdin process) (getStdout process) mem
-
-    let reqMap = undefined
-    rawCmds <- timedCallandResponse 100 (getStdin process) (getStdout process) reqMap
-    newMem <- timedCallandResponse 100 (getStdin process) (getStdout process) ""
-    hClose (getStdin process)
-
-    exitCode <- timedWaitExitCode 100 process
-
-    errormsg' <- T.pack <$> hGetContents (getStderr process)
-    errormsg <- CE.evaluate $ force errormsg'
-
-    return $ bot { errorLog = errormsg
-                 , commands = undefined -- rawCmds
-                 , memory = newMem
-                 }
-takeTurn _ bot = return bot
+takeTurn :: Map.Map -> Bot -> IO Bot
+takeTurn _ (Crashed a b c d) = return (Crashed a b c d)
+takeTurn map (NewBot a b c) = return (takeTurn map (initialize (NewBot a b c)))
+takeTurn map b = withProcessWait botConfig $ \p -> execBot p b $ do
+  bot <- get
+  liftIO $ hPutStrLn (getStdin p) (memory b)
+  liftIO $ hFlush (getStdin p)
+  response <- tryIO "Timeout while waiting for map request." $ timeout 1000 (hGetLine (getStdout p))
+  put $ bot {mapReq = Just ((1, 1), 1)}
+  liftIO $ hPutStrLn (getStdin p) (show map)
+  liftIO $ hFlush (getStdin p)
+  response <- tryIO "Timeout while waiting for commands." $ timeout 1000 (hGetLine (getStdout p))
+  return ()
   where
     botConfig = setStdin createPipe
               $ setStdout createPipe
               $ setStderr createPipe
-              $ setWorkingDir (dropFileName fp)
-              $ fromString fp
+              $ setWorkingDir (dropFileName (filePath b))
+              $ fromString (filePath b)
 
