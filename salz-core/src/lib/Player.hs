f677cefa6af0134babf0a93bae65f858e6b6df99
Lukas Schmidt <lfschmidt.me@gmail.com>
2020-04-13 10:18:22 -0400

    [salz-core] Update Player to new map format

--- salz-core/src/lib/Player.hs
+++ salz-core/src/lib/Player.hs
@@ -1,44 +1,31 @@
 module Player ( applyCommands
-              , getPlayerCells
-              , filterLegalCommands
               ) where
 
+import qualified Map as Map
+import Data.List
 
-
-getPlayerCells :: Board h w CellInfo -> PlayerId -> [Cell h w CellInfo]
-getPlayerCells b pi1 = getCellsBy b (\x -> pi1 == (cPlayerId x))
-
-getOtherPlayersCells :: Board h w CellInfo -> PlayerId -> [Cell h w CellInfo]
-getOtherPlayersCells b pi1 = getCellsBy b (\x -> pi1 /= (cPlayerId x))
-
-isMyNeighbour :: (KnownNat w, KnownNat h) => Board w h CellInfo -> PlayerId -> Cell w h CellInfo -> Bool
-isMyNeighbour board pid cell = any (\x -> dist x cell < 3) $ getPlayerCells board pid
-
-isNearEnemy :: (KnownNat w, KnownNat h) => Board w h CellInfo -> Cell w h CellInfo -> Bool
-isNearEnemy board cell = any (\x -> dist x cell < 30) $ getOtherPlayersCells board (cPlayerId  $ cItem cell)
-
-isCommandLegal :: (KnownNat w, KnownNat h) => Board w h CellInfo -> PlayerId -> Command -> Bool
-isCommandLegal board pid (Flip x y) = isMyNeighbour board pid cell && (not $ isNearEnemy board cell)
+applyCommands :: Map.Map -> [(Map.Coord, Int)] -> Map.Map
+applyCommands map cmds = foldl applyCommand map $ filterLegalCommands map cmds
   where
-    cell = Cell (toMod x) (toMod y) (CellInfo pid)
+    applyCommand :: Map.Map -> (Map.Coord, Int) -> Map.Map
+    applyCommand (Map.M mlst) cel = if length (delete cel mlst) /= length mlst
+                                    then Map.M $ cel:mlst
+                                    else Map.M $ delete cel mlst
 
-getLegalCommands :: (KnownNat w, KnownNat h) => Board w h CellInfo -> [(PlayerId, [Command])] -> [(PlayerId, [Command])]
-getLegalCommands board cmds = map getLegalCommand cmds
-  where
-    getLegalCommand (p, clst) = (p, filter (isCommandLegal board p) $ take 3 clst)
+filterLegalCommands :: Map.Map -> [(Map.Coord, Int)] -> [(Map.Coord, Int)]
+filterLegalCommands map cmds = filter (isCommandLegal map) cmds
 
-filterLegalCommands :: (KnownNat w, KnownNat h) => Board w h CellInfo -> [(PlayerId, [Command])] -> [(PlayerId, Int, Int)]
-filterLegalCommands board cmds = foldr foldHelper [] $ getLegalCommands board cmds
-  where
-    foldHelper :: (PlayerId, [Command]) -> [(PlayerId, Int, Int)] -> [(PlayerId, Int, Int)]
-    foldHelper (pid, lst) res = (map (\(Flip x y) -> (pid, x, y)) lst) ++ res
+isCommandLegal :: Map.Map -> (Map.Coord, Int) -> Bool
+isCommandLegal map (c, pid) = isPlayerNeighbour map pid c && (not $ isNearEnemy map pid c)
 
-applyFlips :: (KnownNat w, KnownNat h) => Board w h CellInfo -> [(PlayerId, [Command])] -> Board w h CellInfo
-applyFlips board cmds = foldl togglePlayerCells board cmds
-  where
-    togglePlayerCells board1 (pid, clst) = foldl toggleCell board1 $ map (\(Flip x y) -> Cell (toMod x) (toMod y) (CellInfo pid)) clst
+isPlayerNeighbour :: Map.Map -> Int -> Map.Coord -> Bool
+isPlayerNeighbour map pid c = any (\(x, _) -> Map.dist x c <= 1) $ getPlayerCells map pid
 
-applyCommands :: (KnownNat w, KnownNat h) => Board w h CellInfo -> [(PlayerId, [Command])] -> Board w h CellInfo
-applyCommands board cmds = applyFlips board $ getLegalCommands board cmds
+isNearEnemy :: Map.Map -> Int -> Map.Coord -> Bool
+isNearEnemy map pid c = any (\(x, _) -> Map.dist x c <= 30) $ getOtherPlayerCells map pid
 
+getPlayerCells :: Map.Map -> Int -> [(Map.Coord, Int)]
+getPlayerCells (Map.M mlst) pid = filter (\(_, x) -> x == pid) mlst
 
+getOtherPlayerCells :: Map.Map -> Int -> [(Map.Coord, Int)]
+getOtherPlayerCells (Map.M mlst) pid = filter (\(_, x) -> x /= pid) mlst
