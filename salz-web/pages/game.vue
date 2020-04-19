96630fc5d23e3d78adcc0bae42c3c5836e1a5abc
Japorized <johnson5756@live.com>
2020-04-19 20:32:44 +0800

    fix merge problems from rebase

--- salz-web/pages/game.vue
+++ salz-web/pages/game.vue
@@ -5,6 +5,7 @@
     <Hotkeys :viewport="viewport" />
     <Ranking v-if="showRanking" />
     <Help v-if="showHelp" />
+    <Button label="Iterate" @click="iterate" class="iterate">Iterate</Button>
   </div>
 </template>
 
@@ -20,6 +21,13 @@
   padding: 20px;
   position: fixed;
 }
+
+.iterate {
+  background: red !important;
+  position: absolute;
+  top: 50px;
+  z-index: 9999;
+}
 </style>
 
 <script charset="utf-8">
@@ -28,17 +36,21 @@ import { Application } from 'pixi.js';
 import { Viewport } from 'pixi-viewport';
 
 // Game Rendering
-import GameFrame from '~/lib/game/rendering/gameFrame';
-import Frame from '~/lib/game/rendering/frame';
-import { Color } from '~/lib/game/colors.js';
-import { sketchyMedoid } from '~/lib/game/mathstuff';
-import { EventBus } from '~/lib/eventBus';
+import GameFrame from '@/lib/game/rendering/gameFrame';
+import Frame from '@/lib/game/rendering/frame';
+import Cell from '@/lib/game/entities/cell';
+import { Color } from '@/lib/game/colors.js';
+import { sketchyMedoid } from '@/lib/game/mathstuff';
+import { EventBus } from '@/lib/eventBus';
 
-import GameMenu from '~/components/Game/GameMenu';
-import FrameControl from '~/components/Game/FrameControl';
-import Hotkeys from '~/components/Game/Hotkeys';
-import Ranking from '~/components/Game/Ranking';
-import Help from '~/components/Game/Help';
+import GameMenu from '@/components/Game/GameMenu';
+import FrameControl from '@/components/Game/FrameControl';
+import Hotkeys from '@/components/Game/Hotkeys';
+import Ranking from '@/components/Game/Ranking';
+import Help from '@/components/Game/Help';
+import Button from '@/components/form/Button';
+
+import Grid from '@/lib/game/conway';
 
 export default {
   components: {
@@ -47,11 +59,24 @@ export default {
     Hotkeys,
     Ranking,
     Help,
+    Button,
   },
   data() {
     return {
-      index: [],
+      turn: 0,
+      grid: new Grid(0, 0),
       viewport: null,
+      cellcolors: [
+        Color.pink,
+        Color.red,
+        Color.orange,
+        Color.yellow,
+        Color.green,
+        Color.turquiose,
+        Color.cyan,
+        Color.blue,
+        Color.purple,
+      ],
     };
   },
   computed: {
@@ -64,83 +89,34 @@ export default {
       showRanking: state => state.game.showRanking,
       showHelp: state => state.game.showHelp,
     }),
+    snapshots: () => require('@/dummy-data/snapshots').default,
+    moves: () => require('@/dummy-data/moves').default,
   },
-  asyncData({ $axios }) {
-    return $axios
-      .$get('/frames')
-      .then(res => {
-        return { index: res };
-      })
-      .catch(e => {
-        // eslint-disable-next-line
-        console.warn(e);
-        return { index: { frames: [] } };
-      });
-  },
+  // asyncData({ $axios }) {
+  //   return $axios
+  //     .$get('/frames')
+  //     .then((res) => {
+  //       return { index: res };
+  //     })
+  //     .catch((e) => {
+  //       // eslint-disable-next-line
+  //       console.warn(e);
+  //       return { index: { frames: [] } };
+  //     });
+  // },
   beforeMount() {
     this.$store.dispatch('login/grabToken');
   },
   async mounted() {
     const wrapper = document.querySelector('#salz-game-inner-view');
 
-    // HACK -- This is a temporary solution
-    // set a random color for each player
-    const cellcolors = [
-      Color.pink,
-      Color.red,
-      Color.orange,
-      Color.yellow,
-      Color.green,
-      Color.turquiose,
-      Color.cyan,
-      Color.blue,
-      Color.purple,
-    ];
-    const playerColorDict = {};
-    this.index.frames.forEach(frame => {
-      frame.forEach(player => {
-        const id = player.playerid;
-        if (typeof playerColorDict[id] === 'undefined') {
-          playerColorDict[id] = cellcolors[Math.floor(Math.random() * cellcolors.length)];
-        }
-        player.color = playerColorDict[id];
-      });
-    });
-
-    this.$store.dispatch('game/setFramesLength', this.index.frames.length);
-    this.$store.dispatch('game/setFirstFrame', 0);
-    this.$store.dispatch('game/setActiveFrame', 0);
-    if (this.index.frames.length > 0) {
-      this.$store.dispatch('game/setLastFrame', this.index.frames.length - 1);
-      this.$store.dispatch('game/setTurnId', this.index.frames[0][0].turnid);
-    } else {
-      this.$store.dispatch('game/setLastFrame', 0);
-      this.$store.dispatch('game/setTurnId', 0);
-    }
-
-    const frames = [];
-    // make first n frames first
-    // adjust for lower n for better performance,
-    // but possibly at the cost of exposing incomplete preparation
-    const criticalFrames = Math.min(10, this.index.frames.length);
-    if (this.index.frames.length > 0) {
-      for (let i = 0; i < criticalFrames; i++) {
-        frames.push(new Frame(this.index.frames[i]));
-      }
-    }
-
-    // figure out the dimensions for the canvas
-    const appWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    const appHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-
     const app = new Application({
-      width: appWidth,
-      height: appHeight,
       backgroundColor: Color.black,
       resizeTo: wrapper,
       resolution: window.devicePixelRatio || 1,
     });
     wrapper.appendChild(app.view);
+    // this.gameViewport = app.view;
 
     const vpWorldWidth = 1000;
     const vpWorldHeight = 1000;
@@ -154,16 +130,9 @@ export default {
     });
 
     // Set the centre to be the mediod of player's cells
-    // const user = JSON.parse(sessionStorage.getItem('user'));
-    const playerID = parseInt(this.userid, 10);
+    // const playerID = parseInt(this.userid, 10);
 
-    if (playerID == null || this.index.frames.length === 0) {
-      this.viewport.moveCenter(0, 0);
-    } else {
-      // camera should point at middle of cluster.
-      this.viewport.moveCenter(...sketchyMedoid(playerID, this.index.frames[0]));
-    }
-
+    this.viewport.moveCenter(0, 0);
     app.stage.addChild(this.viewport);
     this.viewport
       .drag()
@@ -176,28 +145,15 @@ export default {
         maxWidth: vpWorldWidth * 5,
         maxHeight: vpWorldHeight * 5,
       });
+    this.grid = await new Grid(50, 50);
+    await this.grid.mountSnapshot(this.snapshots[0]);
 
-    const gameFrame = await new GameFrame(
-      this.index.frames.length > 0
-        ? frames[0]
-        : new Frame([
-            {
-              turnid: 0,
-              playerid: 0,
-              pos: [],
-            },
-          ]),
-      vpWorldWidth,
-      vpWorldHeight,
-    );
-    this.viewport.addChild(gameFrame);
-
-    // continue making the rest of the frames here
-    if (this.index.frames.length > 0) {
-      for (let i = criticalFrames; i < this.index.frames.length; i++) {
-        frames.push(new Frame(this.index.frames[i]));
+    this.grid.cells.forEach(cell => {
+      if (cell.isAlive) {
+        this.viewport.addChild(new Cell(cell.owner, cell.x, cell.y, this.cellcolors[0]));
       }
-    }
+    });
+    this.turn = this.snapshots[0].turnid;
 
     // Add a helper function for viewport
     /**
@@ -210,9 +166,26 @@ export default {
       this.viewport.moveCenter(this.viewport.center.x + dx, this.viewport.center.y + dy);
     };
 
-    EventBus.$on('updateFrameIndex', () => {
-      gameFrame.mountFrame(frames[this.currentFrameNumber]);
-    });
+    // EventBus.$on('updateFrameIndex', () => {
+    //   gameFrame.mountFrame(frames[this.currentFrameNumber]);
+    // });
+  },
+  methods: {
+    iterate() {
+      const nextMove = this.moves.filter(move => move.turnid === this.turn);
+      if (nextMove) {
+        this.grid.next(nextMove[0]);
+        this.updateCanvas();
+      }
+    },
+    updateCanvas() {
+      this.viewport.removeChildren();
+      this.grid.cells.forEach(cell => {
+        if (cell.isAlive) {
+          this.viewport.addChild(new Cell(cell.owner, cell.x, cell.y, this.cellcolors[0]));
+        }
+      });
+    },
   },
 };
 </script>
