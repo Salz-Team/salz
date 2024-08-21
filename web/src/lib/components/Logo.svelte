a8c4f5772d31729cd10626b65280d1657b10124a
Japorized <johnson5756@live.com>
2024-08-21 18:50:09 -0400

    (web) Add component Logo

---
+++ web/src/lib/components/Logo.svelte
@@ -0,0 +1,26 @@
+<svg
+	version="1.1"
+	viewBox="0 0 120 120"
+	xmlns="http://www.w3.org/2000/svg"
+	class="logo"
+	preserveAspectRatio="xMinYMin meet"
+>
+	<rect x="40" y="0" />
+	<rect x="80" y="40" />
+	<rect x="0" y="80" />
+	<rect x="40" y="80" />
+	<rect x="80" y="80" />
+</svg>
+
+<style>
+	.logo {
+		fill: black;
+    width: 1.2em;
+	}
+
+	.logo rect {
+		width: 33.33%;
+		height: 33.33%;
+		fill: inherit;
+	}
+</style>
