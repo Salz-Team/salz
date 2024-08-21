e1fb881f5569e23bc4d00a527a4a1fc74542cc67
Japorized <johnson5756@live.com>
2024-08-21 19:04:36 -0400

    (web) Add some api calling methods

---
+++ web/src/lib/api/api.ts
@@ -0,0 +1,25 @@
+import { get } from '$lib/req';
+import { None } from 'rust-optionals';
+
+export const getHealth = (apiBaseurl: string) =>
+	get(new URL('/health', apiBaseurl), None(), {}, { mode: 'cors' });
+
+export const login = async (apiBaseurl: string): Promise<string | null> => {
+	const resp = await get(
+		new URL('/login', apiBaseurl),
+		None(),
+		{},
+		{ redirect: 'manual', mode: 'cors' },
+	);
+	if (resp.isOk()) {
+		return resp.unwrap().headers.get('location');
+	} else {
+		return null;
+	}
+};
+
+export const getMe = (apiBaseurl: string) =>
+  get(new URL("/users/me", apiBaseurl), None(), {}, { mode: 'cors' });
+
+export const getUsers = (apiBaseurl: string) =>
+  get(new URL("/users", apiBaseurl), None(), {}, { mode: 'cors' });
