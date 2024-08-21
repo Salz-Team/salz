fdbb7894d602a9f7e08c60385ffe117c8e6ebe11
Japorized <johnson5756@live.com>
2024-08-21 18:52:53 -0400

    (web) Add default layout for all other pages

---
+++ web/src/routes/(all_pages)/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Nav from '$lib/components/Nav.svelte';
+</script>
+
+<Nav />
+<div class="content-container"><slot></slot></div>
+
+<style>
+  .content-container {
+    padding: 0 2em;
+  }
+</style>
