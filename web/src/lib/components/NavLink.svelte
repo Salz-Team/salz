9803a14ab269113013fb084a1591c38000ba3d2c
Japorized <johnson5756@live.com>
2024-08-21 18:50:01 -0400

    (web) Add component NavLink

---
+++ web/src/lib/components/NavLink.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let href: string;
+	export let canBeActivated: boolean = true;
+	export let useExactPath: boolean = false;
+
+	$: shouldBeActive =
+		canBeActivated &&
+		(!useExactPath ? $page.url.pathname.startsWith(href) : $page.url.pathname === href);
+</script>
+
+<li class="navlink" class:active={shouldBeActive}>
+	<a {href}>
+		<slot></slot>
+	</a>
+</li>
+
+<style>
+	a:link,
+	a:visited {
+		text-decoration: none;
+		color: inherit;
+	}
+
+	a:link {
+		--slant-px: 10px;
+		padding: 0.6em 1em;
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
+		background: lightgray;
+		transition:
+			background 0.2s ease-in-out,
+			filter 0.2s ease-in-out;
+		clip-path: polygon(var(--slant-px) 0, 100% 0, calc(100% - var(--slant-px)) 100%, 0 100%);
+	}
+
+	li.active a:link {
+		background: var(--accent-color);
+		background-size: 400% 200%;
+		animation: background-scroll 5s linear infinite;
+	}
+
+	a:hover {
+		cursor: pointer;
+		filter: brightness(75%);
+	}
+
+	a:focus {
+		background: var(--accent-color);
+		background-size: 400% 200%;
+		animation: background-scroll 5s linear infinite;
+	}
+</style>
