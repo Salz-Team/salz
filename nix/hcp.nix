6bc21424f413b5eaa4cc2856bf951785c649a700
Lukas Schmidt <lfschmidt.me@gmail.com>
2024-08-20 21:11:35 -0400

    Add darwin arm hcp package

--- nix/hcp.nix
+++ nix/hcp.nix
@@ -3,15 +3,23 @@
 , fetchurl
 , unzip
 }:
-
-stdenv.mkDerivation rec {
+let
   pname = "hcp";
   version = "0.4.0";
-
-  src = fetchurl {
-    url = "https://releases.hashicorp.com/${pname}/${version}/${pname}_${version}_linux_amd64.zip";
-    hash = "sha256-9g7ZO1fXPT1fom31mwncOxb5tO4urf72tasha2KRM1I=";
+  sources = {
+    "x86_64-linux" = {
+      url = "https://releases.hashicorp.com/${pname}/${version}/${pname}_${version}_linux_amd64.zip";
+      hash = "sha256-9g7ZO1fXPT1fom31mwncOxb5tO4urf72tasha2KRM1I=";
+    };
+    "arm_64-darwin" = {
+      url = "https://releases.hashicorp.com/${pname}/${version}/${pname}_${version}_linux_arm64.zip";
+      hash = "";
+    };
   };
+in stdenv.mkDerivation rec {
+  inherit pname version;
+
+  src = fetchurl sources."${stdenv.hostPlatform.system}";
 
   sourceRoot = ".";
 
@@ -23,4 +31,8 @@ stdenv.mkDerivation rec {
     mkdir -p $out/bin
     cp hcp $out/bin
   '';
+
+  meta = with lib; {
+    platforms = with platforms; [ "x86_64-linux" "arm_64-darwin" ];
+  };
 }
