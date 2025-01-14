b4595a0587f7cdb053f55bb883d55638550fc7a1
Lukas Schmidt <lfschmidt.me@gmail.com>
2025-01-13 21:16:41 -0500

    Fixme 2

--- game-pipeline/TicTacToe.hs
+++ game-pipeline/TicTacToe.hs
@@ -8,22 +8,19 @@ import Data.Aeson
 import Data.Aeson.Types (Parser)
 import GHC.Generics
 
--- Define the player markers
+-- Types
+
 data Player = X | O deriving (Eq, Show, Generic, FromJSON, ToJSON)
-
-playerToInt X = 0
-playerToInt O = 1
-
-intToPlayer 0 = X
-intToPlayer 1 = O
-
-flushedPutStrLn line = do
-  putStrLn line
-  hFlush stdout
 
 type Board = [[Maybe Player]]
 
-data PlayerResponse = PlayerResponse Int8 Int8 deriving (Show)
+data PlayerResponse = PlayerResponse Int Int deriving (Show)
+
+data GameEngineInMessage = GameStart Int | PlayerResponses [(Int, Bool, PlayerResponse)] deriving (Show)
+
+data GameEngineOutMessage = GameEnd [(Int, Float)] | PlayerTurn [(Int, Board)] | DebugMessage String deriving (Show)
+
+-- JSON Parsers
 
 instance FromJSON PlayerResponse where
   parseJSON = withObject "PlayerResponse" $ \obj -> do
@@ -34,10 +31,8 @@ instance FromJSON PlayerResponse where
       else
         fail "Coordinates out of bounds"
     where
-      inRange :: Int8 -> Int8 -> Bool
+      inRange :: Int -> Int -> Bool
       inRange x y = x >= 0 && x <= 2 && y >= 0 && y <= 2
-
-data GameEngineInMessage = GameStart Int | PlayerResponses [(Int, Bool, PlayerResponse)] deriving (Show)
 
 instance FromJSON GameEngineInMessage where
   parseJSON = withObject "GameEngineInMessage" $ \obj -> do
@@ -56,8 +51,6 @@ instance FromJSON GameEngineInMessage where
         response <- obj .: "response"
         isValid <- obj .: "isValid"
         return (player, isValid, response)
-
-data GameEngineOutMessage = GameEnd [(Int, Float)] | PlayerTurn [(Int, Board)] | DebugMessage String deriving (Show)
 
 instance ToJSON GameEngineOutMessage where
   toJSON (DebugMessage message) = object [
@@ -107,45 +100,25 @@ isFull = all (all isJust)
     isJust (Just _) = True
     isJust Nothing  = False
 
--- Main game loop
-playGame :: Board -> GameEngineInMessage -> IO ()
-playGame board (GameStart player) = do
-    flushedPutStrLn $ encode $ PlayerTurn [(player, board)]
-    imsg <- liftEither =<< eitherDecode =<< getLine
-    (newBoard, omsg) <- =<< (playGame_ board imsg)
-    playGame board imsg
+checkGameStart :: GameEngineInMessage -> Either String Int
+checkGameStart (GameStart i) = Right i
+checkGameStart _ = Left "Expected game start message"
 
-playGame board imsg = do
-    (newBoard, omsg) <- (playGame_ board imsg) >>= fromEither
-    flushedPutStrLn $ encode $ omsg
-    newImsg <- eitherDecode >>= getLine
-    playGame newBoard newImsg
-
-playGame_ :: Board -> GameEngineInMessage -> Either String (Board, GameEngineOutMessage)
-playGame_ board msg_ = do
-    msg <- msg_
-    (player, playerResponse) <- case msg of
-      (PlayerResponses [(player, True, response)]) -> Right (intToPlayer player, response)
-      _ -> Left "Expected one valid player response"
-
-    isValidMove board playerResponse
-    let newBoard = updateBoard board player playerResponse
-    let otherPlayer = nextPlayer player
-    if checkWin player newBoard
-      then
-        Right $ GameEnd [(playerToInt player, 1), (playerToInt otherPlayer, 0)]
-      else
-        Right $ PlayerTurn [(otherPlayer, newBoard)]
+checkPlayerResponse :: GameEngineInMessage -> Either String PlayerResponse
+checkPlayerResponse (PlayerResponses [(_, True, pr)]) = Right pr
+checkPlayerResponse _ = Left "Expected player responses"
 
 -- Check if the move is valid (within bounds and cell is empty)
 isValidMove :: Board -> PlayerResponse -> Either String Bool
-isValidMove board (PlayerResponse row col) =
-    row >= 0 && row < 3 &&
-    col >= 0 && col < 3 &&
+isValidMove board (PlayerResponse row col) = do
+    if row >= 0 && row < 3 && col >= 0 && col < 3
+      then
+        Right True
+      else
+        Left "Invalid Move"
     case board !! row !! col of
         Nothing -> Right True
         _       -> Left "Invalid move"
-isValidMove _ _ = Left "Expecting a player response"
 
 -- Update the board with the player's move
 updateBoard :: Board -> Player -> PlayerResponse -> Board
@@ -163,9 +136,40 @@ liftEither :: Either String a -> IO  a
 liftEither (Left errmsg) = error errmsg
 liftEither (Right a) = return a
 
+flushedPutStrLn line = do
+  putStrLn line
+  hFlush stdout
+
+intToPlayer 0 = X
+intToPlayer 1 = O
+
+playerToInt X = 0
+playerToInt O = 1
+
+otherPlayer X = O
+otherPlayer O = X
+
+applyMove :: Board -> Player -> PlayerResponse -> Either String Board
+applyMove board player playerResponse = do
+    isValidMove board playerResponse
+    updateBoard board player playerResponse
+
+-- Main game loop
+playGame :: Board -> Player -> IO ()
+playGame board player
+  | checkWin X board = flushedPutStrLn $ encode $ GameEnd [(playerToInt X, 1), (playerToInt O, 0)]
+  | checkWin O board = flushedPutStrLn $ encode $ GameEnd [(playerToInt O, 1), (playerToInt X, 0)]
+  | otherwise = do
+    flushedPutStrLn $ encode $ PlayerTurn [(player, board)]
+    imsg <- liftEither =<< eitherDecode =<< getLine
+    pr <- checkPlayerResponse imsg
+    newboard <- liftEither applyMove board player pr
+    playGame newboard (otherPlayer player)
 
 -- Start the game
 main :: IO ()
 main = do
-  message <- liftEither =<< decodeEither <$> getLine
-  playGame initialBoard message
+  message <- liftEither =<< eitherDecode <$> getLine
+  liftEither (checkGameStart message)
+  playGame initialBoard X
+
