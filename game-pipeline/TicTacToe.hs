20dcbe8177ef02ecda2a280f7a543fb74d6d071a
Lukas Schmidt <lfschmidt.me@gmail.com>
2025-01-25 21:33:34 -0500

    Add bad tic tac toe player

--- game-pipeline/TicTacToe.hs
+++ game-pipeline/TicTacToe.hs
@@ -1,5 +1,6 @@
 module Main where
 
+
 import Data.List (intercalate)
 import Data.Int (Int8)
 import qualified Data.Vector as V
@@ -11,73 +12,51 @@ import GHC.Generics
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy as LB
 
--- Types
-
-data Player = X | O deriving (Eq, Show, Generic, FromJSON, ToJSON)
-
-type Board = [[Maybe Player]]
-
-data PlayerResponse = PlayerResponse Int Int deriving (Show)
-
--- JSON Parsers
-
-instance FromJSON PlayerResponse where
-  parseJSON = withObject "PlayerResponse" $ \obj -> do
-    [x, y] <- obj .: "move"
-    if inRange x y
-      then
-        return (PlayerResponse x y)
-      else
-        fail "Coordinates out of bounds"
-    where
-      inRange :: Int -> Int -> Bool
-      inRange x y = x >= 0 && x <= 2 && y >= 0 && y <= 2
-
 -- Initial empty board
-initialBoard :: Board
+initialBoard :: TicTacToeBoard
 initialBoard = replicate 3 (replicate 3 Nothing)
 
 -- Check if a player has won
-checkWin :: Player -> Board -> Bool
+checkWin :: TicTacToePlayer -> TicTacToeBoard -> Bool
 checkWin player board =
     any checkRow board ||        -- Check rows
     any checkRow (columns board) ||  -- Check columns
     checkDiagonals board         -- Check diagonals
   where
-    checkRow :: [Maybe Player] -> Bool
+    checkRow :: [Maybe TicTacToePlayer] -> Bool
     checkRow row = all (== Just player) row
 
-    columns :: Board -> Board
+    columns :: TicTacToeBoard -> TicTacToeBoard
     columns = foldr (zipWith (:)) (repeat [])
 
-    checkDiagonals :: Board -> Bool
+    checkDiagonals :: TicTacToeBoard -> Bool
     checkDiagonals bd = all (== Just player) (diag1 bd) || all (== Just player) (diag2 bd)
 
-    diag1 :: Board -> [Maybe Player]
+    diag1 :: TicTacToeBoard -> [Maybe TicTacToePlayer]
     diag1 bd = [bd !! i !! i | i <- [0..length bd - 1]]
 
-    diag2 :: Board -> [Maybe Player]
+    diag2 :: TicTacToeBoard -> [Maybe TicTacToePlayer]
     diag2 bd = [bd !! i !! (length bd - 1 - i) | i <- [0..length bd - 1]]
 
 -- Check if the board is full (tie game)
-isFull :: Board -> Bool
+isFull :: TicTacToeBoard -> Bool
 isFull = all (all isJust)
   where
-    isJust :: Maybe Player -> Bool
+    isJust :: Maybe TicTacToePlayer -> Bool
     isJust (Just _) = True
     isJust Nothing  = False
 
-checkGameStart :: GameEngineInMessage PlayerResponse -> Either String Int
+checkGameStart :: GameEngineInMessage TicTacToePlayerResponse -> Either String Int
 checkGameStart (GameStart i) = Right i
 checkGameStart _ = Left "Expected game start message"
 
-checkPlayerResponse :: GameEngineInMessage PlayerResponse -> Either String PlayerResponse
+checkPlayerResponse :: GameEngineInMessage TicTacToePlayerResponse -> Either String TicTacToePlayerResponse
 checkPlayerResponse (PlayerResponses [(_, True, pr)]) = Right pr
 checkPlayerResponse _ = Left "Expected player responses"
 
 -- Check if the move is valid (within bounds and cell is empty)
-isValidMove :: Board -> PlayerResponse -> Either String Bool
-isValidMove board (PlayerResponse row col) = do
+isValidMove :: TicTacToeBoard -> TicTacToePlayerResponse -> Either String Bool
+isValidMove board (TicTacToePlayerResponse col row) = do
     if row >= 0 && row < 3 && col >= 0 && col < 3
       then
         Right True
@@ -88,14 +67,14 @@ isValidMove board (PlayerResponse row col) = do
         _       -> Left "Invalid move"
 
 -- Update the board with the player's move
-updateBoard :: Board -> Player -> PlayerResponse -> Board
-updateBoard board player (PlayerResponse row col) =
+updateBoard :: TicTacToeBoard -> TicTacToePlayer -> TicTacToePlayerResponse -> TicTacToeBoard
+updateBoard board player (TicTacToePlayerResponse col row) =
     take row board ++
     [take col (board !! row) ++ [Just player] ++ drop (col + 1) (board !! row)] ++
     drop (row + 1) board
 
 -- Determine the next player
-nextPlayer :: Player -> Player
+nextPlayer :: TicTacToePlayer -> TicTacToePlayer
 nextPlayer X = O
 nextPlayer O = X
 
@@ -119,16 +98,16 @@ playerToInt O = 1
 otherPlayer X = O
 otherPlayer O = X
 
-applyMove :: Board -> Player -> PlayerResponse -> Either String Board
+applyMove :: TicTacToeBoard -> TicTacToePlayer -> TicTacToePlayerResponse -> Either String TicTacToeBoard
 applyMove board player playerResponse = do
     isValidMove board playerResponse
     return $ updateBoard board player playerResponse
 
 -- Main game loop
-playGame :: Board -> Player -> IO ()
+playGame :: TicTacToeBoard -> TicTacToePlayer -> IO ()
 playGame board player
-  | checkWin X board = flushedPutStrLnB $ encode $ (GameEnd [(playerToInt X, 1), (playerToInt O, 0)] :: GameEngineOutMessage Board)
-  | checkWin O board = flushedPutStrLnB $ encode $ (GameEnd [(playerToInt O, 1), (playerToInt X, 0)] :: GameEngineOutMessage Board)
+  | checkWin X board = flushedPutStrLnB $ encode $ (GameEnd [(playerToInt X, 1), (playerToInt O, 0)] :: GameEngineOutMessage TicTacToeBoard)
+  | checkWin O board = flushedPutStrLnB $ encode $ (GameEnd [(playerToInt O, 1), (playerToInt X, 0)] :: GameEngineOutMessage TicTacToeBoard)
   | otherwise = do
     flushedPutStrLnB $ encode $ PlayerTurn [(playerToInt player, board)]
     imsg <- liftEither =<< eitherDecode <$> LB.fromStrict <$> B.getLine
---
+++ game-pipeline/TicTacToePlayer.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import Options.Applicative
+import Debug.Trace
+import Control.Monad
+import System.Process
+import System.IO
+import System.Exit
+import Data.Maybe
+import Data.List
+import Data.Aeson
+import GHC.Generics
+import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy as LB
+
+import Parsers
+
+liftEither :: Either String a -> IO  a
+liftEither (Left errmsg) = error errmsg
+liftEither (Right a) = return a
+
+
+flushedPutStrLnB handle line = do
+  B.hPutStrLn handle (LB.toStrict line)
+  hFlush handle
+
+play :: TicTacToeBoard -> TicTacToePlayerResponse
+play board = fromJust $ do
+  y <- findIndex (any isNothing) board
+  x <- findIndex isNothing (board !! y)
+  return $ TicTacToePlayerResponse x y
+
+main :: IO ()
+main = do
+  board <- liftEither =<< eitherDecode <$> LB.fromStrict <$> B.getLine
+  flushedPutStrLnB stdout $ encode $ play board
+  main
