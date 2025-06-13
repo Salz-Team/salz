8efb58b6bc7d7e2fb69b971f0ed4aa34f23ad5c3
Dillon Chan <dillon@mosiman.ca>
2025-06-13 13:09:23 -0400

    (misc) flake updates

--- flake.nix
+++ flake.nix
@@ -1,52 +1,61 @@
 { description = "Salz";
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs, flake-utils }:
-        flake-utils.lib.eachDefaultSystem (system:
+    outputs = { self, nixpkgs }:
             let
-                pkgs = import nixpkgs {
-                    inherit system;
-                    config = { allowUnfree = true; };
-                };
+                forAllSystems = function:
+                    nixpkgs.lib.genAttrs [
+                        "x86_64-linux"
+                        "aarch64-linux"
+                    ] (
+                        system:
+                        function system (import nixpkgs {
+                            inherit system;
+                            config.allowUnfree = true;
+                            overlays = [];
+                        }));
+                forAllSystemsPkgsOnly = function: (forAllSystems (_: pkgs: function pkgs));
             in {
-                apps = {
-                    api = flake-utils.lib.mkApp {
-                        drv = pkgs.writeShellApplication {
-                            name = "api";
-                            runtimeInputs = [ pkgs.go (pkgs.callPackage ./nix/hcp.nix{}) ];
-                            text = ''
-                                pushd ./api > /dev/null && ./run.sh
-                            '';
-                        };
+                packages = forAllSystemsPkgsOnly (pkgs: {
+                    api = pkgs.buildGoModule (finalAttrs: {
+                        pname = "api";
+                        version = "0.0.1";
+                        src = ./api;
+                        vendorHash = "sha256-tgWC6WTyP+juEF1t8yv1ZT4j2O5+YZzwI8JQ9CUHads=";
+                    });
+                });
+
+                devShells = forAllSystems (system: pkgs: {
+                    default = pkgs.mkShell {
+                        inputsFrom = [
+                            self.packages.${system}.api
+                        ];
+
+                        packages = [
+                            pkgs.git
+                            pkgs.graphviz
+                            pkgs.sqlfluff
+                            pkgs.jsonschema
+                            pkgs.postgresql_16
+                            (pkgs.callPackage ./nix/hcp.nix {})
+                            pkgs.process-compose
+                            pkgs.minio
+                            pkgs.minio-client
+
+                            pkgs.go
+
+                            pkgs.nodePackages.npm
+                            pkgs.nodePackages.typescript-language-server
+                            pkgs.nodePackages.svelte-language-server
+                        ];
+                        shellHook = ''
+                            export GOPATH="$(pwd)/.go"
+                            export GOCACHE=""
+                            export GO111MODULE='on'
+                        '';
                     };
-                };
-
-                devShell = pkgs.mkShell {
-                    buildInputs = [
-                        pkgs.git
-                        pkgs.graphviz
-                        pkgs.sqlfluff
-                        pkgs.jsonschema
-                        pkgs.postgresql_16
-                        (pkgs.callPackage ./nix/hcp.nix {})
-                        pkgs.process-compose
-                        pkgs.minio
-                        pkgs.minio-client
-
-                        pkgs.go
-
-                        pkgs.nodePackages.npm
-                        pkgs.nodePackages.typescript-language-server
-                        pkgs.nodePackages.svelte-language-server
-                    ];
-                    shellHook = ''
-                        export GOPATH="$(pwd)/.go"
-                        export GOCACHE=""
-                        export GO111MODULE='on'
-                    '';
-                };
-            });
+                });
+            };
 }
